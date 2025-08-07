636e6a853e49d29e22b6f4346252f56e142c983f
Sayuru Priyanjana <sayurupriyanjana41@gmail.com>
2025-08-07 20:07:53 +0530

    initial

---
+++ Bodima_backend/src/main/java/com/sayuru/Bodima_backend/BodimaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sayuru.Bodima_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodimaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodimaBackendApplication.class, args);
+	}
+
+}
---
+++ Bodima_backend/src/test/java/com/sayuru/Bodima_backend/BodimaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayuru.Bodima_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodimaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
